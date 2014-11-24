9c4beb2f6033e85abfd49c3cc515806e8dbb46ff
Alex <alexander.orlov@loxal.net>
2014-11-24 06:40:23 +0100

    align with source

--- cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/rest/StreamingLogTokenImpl.java
+++ cloudfoundry-client-lib/src/main/java/org/cloudfoundry/client/lib/rest/StreamingLogTokenImpl.java
@@ -1,24 +1,24 @@
 package org.cloudfoundry.client.lib.rest;
 
-import org.cloudfoundry.client.lib.StreamingLogToken;
-
-import javax.websocket.Session;
 import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class StreamingLogTokenImpl implements StreamingLogToken {
-    // The Go client uses 25 seconds which is not sufficient for some cases
-    private static final long KEEP_ALIVE_TIME = 3 * 25000;
+import javax.websocket.Session;
 
+import org.cloudfoundry.client.lib.StreamingLogToken;
+
+public class StreamingLogTokenImpl implements StreamingLogToken {
+    private static long keepAliveTime = 25000; // 25 seconds to match the go client
+    
     private Timer keepAliveTimer = new Timer(true);
 
     private Session session;
 
     public StreamingLogTokenImpl(Session session) {
         this.session = session;
-
-        keepAliveTimer.scheduleAtFixedRate(new KeepAliveTimerTask(), KEEP_ALIVE_TIME, KEEP_ALIVE_TIME);
+                
+        keepAliveTimer.scheduleAtFixedRate(new KeepAliveTimerTask(), keepAliveTime, keepAliveTime);
     }
     
     public void cancel() {
